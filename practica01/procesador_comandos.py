a06fb4f9b783a344b74dc54cd13c2319b31c8b68
Luz <luzmariarobles18@gmail.com>
2026-04-28 07:59:50 -0600

    asistente_logico 1.1

--- practica01/procesador_comandos.py
+++ practica01/procesador_comandos.py
@@ -1,10 +1,25 @@
 def obtener_saludo(nombre_bot):
-    
+    """
+    Retorna un saludo formateado
+    """
+    return f"Hola, soy {nombre_bot} y estoy listo para ayudarte."
 
 def procesar_comando_recordar(argumento):
-    
+    """
+    Valida y procesa la accion de recordar un dato
+    """
+    if not comando:
+        return "Error: falta el nombre. Uso de !recordar [nombre]"
+    retorn f"¡Entendido! Recordaré el nombre: {comando}"
 
 def calcular_uptime(hora_inicio):
+    """
+    Calcula la diferencias de tiempo entre el inicio 
+    y el actual (mostrar actividad del bot)
+    """
+    ahora = datetime.datetime.now()
+    diferencia = ahora - hora_inicio
+    return f"Tiempo de actividad: {segundos] segundos"
     
 
 def mostrar_ayuda():
@@ -14,7 +29,7 @@ def iniciar_agente():
 
 
 
-import datatime
+import datetime
 
 def main():
     nombre_bot = "AgenteBot"
